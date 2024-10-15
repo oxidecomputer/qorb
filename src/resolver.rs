899a9b85489fb4ec2da56667a2df73e1d40dfe9f
Sean Klein <seanmarionklein@gmail.com>
2024-10-15 12:11:30 -0700

    Add explicit API to terminate pool cleanly

--- src/resolver.rs
+++ src/resolver.rs
@@ -2,6 +2,7 @@
 
 use crate::backend::{self, Backend};
 
+use async_trait::async_trait;
 use std::collections::BTreeMap;
 use std::sync::Arc;
 use tokio::sync::watch;
@@ -13,11 +14,17 @@ pub type AllBackends = Arc<BTreeMap<backend::Name, Backend>>;
 /// The resolver is responsible for knowing which [crate::service::Name]
 /// it is resolving. It is responsible for reporting the set of
 /// all possible backends, but not reporting nor tracking their health.
+#[async_trait]
 pub trait Resolver: Send + Sync {
     /// Start running a resolver.
     ///
     /// Returns a receiver to track ongoing activity.
     fn monitor(&mut self) -> watch::Receiver<AllBackends>;
+
+    /// Cleanly terminates the server.
+    ///
+    /// This ensures that background tasks, if they exist, have stopped.
+    async fn terminate(&mut self) {}
 }
 
 /// Helper type for anything that implements the Resolver interface.
